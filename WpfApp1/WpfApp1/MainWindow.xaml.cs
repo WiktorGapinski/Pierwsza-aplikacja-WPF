fe542ff5636a555f68a37185b6e794faa5398874
Wiktor Gapiński <wiktor_gapinski@proton.me>
2024-11-17 19:03:45 +0100

    Dodałem Plik WPF Zadania 2.1 2.2 2.3 2.4

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.Opacity = 0.5;
+            MessageBox.Show("Witaj, świecie!");
+            btnStart.Opacity = 1.0;
+
+            btnStart.Visibility = Visibility.Hidden;
+            MessageBox.Show("Witaj ponownie, świecie!");
+            btnStart.Visibility = Visibility.Visible;
+        }
+
+        private void btnTime_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DateTime data = DateTime.Now;
+            btnTime.Content = data.ToString("T");
+        }
+
+        private void btnTime_MouseLeave(object sender, MouseEventArgs e)
+        {
+            btnTime.Content = "Czas";
+        }
+
+        private void btnActivateStart_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.IsEnabled = true;
+        }
+    }
+}
